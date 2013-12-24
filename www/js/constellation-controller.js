b40d7270e8c2ea83bd31474e1df279f9d838bec0
Juraj Michalek <juraj.michalek@sinusgear.com>
2013-12-24 14:02:18 +0100

    drag stars to draw lines, with backward animation

--- www/js/constellation-controller.js
+++ www/js/constellation-controller.js
@@ -1,3 +1,4 @@
+/** Touch patch */
 function touchHandler(event)
 {
     var touches = event.changedTouches,
@@ -20,45 +21,31 @@ function touchHandler(event)
 }
 
 angular.module('app', ['ngDragDrop'])
-    .controller('ConstellationController', function($scope, $locale) {
+    .controller('ConstellationController', function($scope, $timeout, $locale) {
+
+        /** Indicate that game mode is running */
+        $scope.isGameMode = false;
 
         $scope.constellations = startConstellations;
         $scope.constellation = {
             stars:[],
             lines:[]
         };
+        $scope.visibleLines = [];
+        $scope.visibleStars = [];
 
         $scope.init = function() {
-            $scope.constellation = $scope.constellations['gemini'];
+            // Touch event fix
             document.addEventListener("touchstart", touchHandler, true);
             document.addEventListener("touchmove", touchHandler, true);
             document.addEventListener("touchend", touchHandler, true);
             document.addEventListener("touchcancel", touchHandler, true);
-//            $('#widget').draggable();
-        };
 
+            $scope.constellation = $scope.constellations['gemini'];
+            $scope.visibleLines = $scope.constellation.lines.slice();
+            $scope.visibleStars = $scope.constellation.stars.slice();
 
-        $scope.dropped = function(dragEl, dropEl) { // function referenced by the drop target
-            //this is application logic, for the demo we just want to color the grid squares
-            //the directive provides a native dom object, wrap with jqlite
-            var drop = angular.element(dropEl);
-            var drag = angular.element(dragEl);
-
-            //clear the previously applied color, if it exists
-            var bgClass = drop.attr('data-color');
-            if (bgClass) {
-                drop.removeClass(bgClass);
-            }
-
-            //add the dragged color
-            bgClass = drag.attr("data-color");
-            drop.addClass(bgClass);
-            drop.attr('data-color', bgClass);
-
-            //if element has been dragged from the grid, clear dragged color
-            if (drag.attr("x-lvl-drop-target")) {
-                drag.removeClass(bgClass);
-            }
+            $timeout($scope.startGame, 1000);
         };
 
         $scope.getStarStyle = function(item) {
@@ -89,5 +76,53 @@ angular.module('app', ['ngDragDrop'])
                 background: "lightyellow"
             };
         };
+
+        $scope.hideLines = function() {
+            $scope.visibleLines = [];
+        };
+
+        $scope.startGame = function() {
+            $scope.visibleLines = $scope.constellation.lines.slice();
+            $scope.hideLines();
+            $scope.isGameMode = true;
+        };
+
+        /** Display line from start to end point. Ignore if line already exists. */
+        $scope.displayLine = function(startPoint, endPoint) {
+            for (var index = 0; index < $scope.visibleLines.length; index++) {
+                var item = $scope.visibleLines[index];
+                // Check whether line already exists
+                if ((startPoint == item[0]) && (endPoint == item[1]) ||
+                    (startPoint == item[1]) && (endPoint == item[0])) {
+                    return;
+                }
+            }
+
+            // Display line
+            $scope.visibleLines.push([startPoint, endPoint]);
+            $scope.visibleStars[startPoint].x = $scope.constellation.stars[startPoint].x;
+            $scope.visibleStars[startPoint].y = $scope.constellation.stars[startPoint].y;
+        };
+
+        $scope.starDrop = function(event, sourceObject) {
+            var sourceIndex = parseInt(event.target.getAttribute('data-index'));
+            var targetIndex = parseInt(event.toElement.getAttribute('data-index'));
+            var forwardVector = [sourceIndex, targetIndex];
+            var reverseVector = [targetIndex, sourceIndex];
+
+            // Check whether drop is valid and display line
+            for (var index = 0; index < $scope.constellation.lines.length; index++) {
+                var item = $scope.constellation.lines[index];
+                var startPoint = item[0];
+                var endPoint = item[1];
+                if ((startPoint == sourceIndex) && (endPoint == targetIndex)) {
+                    $scope.displayLine(startPoint, endPoint);
+                    break;
+                } else if ((startPoint == targetIndex) && (endPoint == sourceIndex)) {
+                    $scope.displayLine(endPoint, startPoint);
+                    break;
+                }
+            }
+        }
     }
 );
